af58c97c3ef8e69f54648a74c0317a67334fa160
aweneagle <aweneagle@gmail.com>
2014-11-18 01:43:38 +0800

    fix mutx

--- go/runner/runner.go
+++ go/runner/runner.go
@@ -8,6 +8,7 @@ package	runner
 
 import	"sync/atomic"
 import	"errors"
+import	"time"
 
 /* 有限容量的信号量 
  * 
@@ -22,7 +23,7 @@ type Mutx struct {
 
 /* 初始化一个信号量 
  */
-func Mutx(ca int32) *Mutx {
+func NewMutx(ca int32) *Mutx {
 	return  &Mutx{ ca:ca, num:0 }
 }
 
@@ -38,9 +39,9 @@ func (m *Mutx) Up () int32 {
 	if m.num >= 0 && new_m <= m.ca && atomic.CompareAndSwapInt32(&m.num, m.num, new_m) {
 		return new_m
 	}
-	if new_m <= m.ca {
+	if new_m <= m.ca && m.num >= 0 {
 		/* 系统繁忙 */
-		return MUXT_ERR_SYS_BUSY
+		return MUTX_ERR_SYS_BUSY
 	} else if m.num < 0 {
 		return MUTX_ERR_CLOSED
 	} else {
@@ -52,42 +53,30 @@ func (m *Mutx) Up () int32 {
 /* down 减少信号量
  */
 func (m *Mutx) Down() int32 {
-	if atomic.AddInt32(&m.num, -1) < 0 {
+	if num := atomic.AddInt32(&m.num, -1) ; num < 0 {
 		return MUTX_ERR_CLOSED
+	} else {
+		return num
 	}
 }
 
 /* 判断信号量是否已经关闭
  */
-func (m *Mutx) IsClosed() {
+func (m *Mutx) IsClosed() bool {
 	return m.num < 0
 }
 
 
 
 type Runner struct {
+	/* Quit 信号量 */
+	q_mutx	*Mutx
 	/* Request 信号量 */
 	r_mutx	*Mutx
-	/* 有多少 routine 在执行 Quit */
-	quitting int32
-
-	/* 有多少 routine 在执行 Request*/
-	running	int32
 
 	/* 终止处理器
 	*/
 	quit	chan bool
-
-	/* 关闭 Request */
-	shutdown	bool
-
-	/* 仍留在 Request() 函数内的 routine，通过 final_quit 告诉 runner 可以关闭服务了 
-	 *
-	 */
-	final_quit	chan bool
-
-	/* 通知 请求routine 数据已经处理完，可以取了
-	 */
 
 	/* 接受 请求routine 的请求
 	 */
@@ -107,15 +96,11 @@ type Event interface {
 /* 初始化一个处理器, 并启动它 */
 func Init () *Runner {
 	p := &Runner{}
-	p.r_mutx = Mutx(65535)
-	p.q_mutx = Mutx(1)
+	p.r_mutx = NewMutx(65535)
+	p.q_mutx = NewMutx(1)
 	p.event_in = make(chan Event)
 	p.event_out = make(chan Event)
 	p.quit = make(chan bool)
-	p.final_quit = make(chan bool)
-	p.running = 0
-	p.quitting = 0
-	p.shutdown = false
 	go p.run()
 	return p
 }
@@ -125,39 +110,53 @@ func Init () *Runner {
  *
  */
 func (p *Runner) Request (in Event) error {
-	if p.r_mutx.Up() > 0 {
-		//println("sending request")
-		p.event_in <- in
-		//println("sending result")
-		_ = <-p.event_out
-		//println("return result...")
-		p.r_mutx.Down()
+	var (
+		m int32
+		slp uint64 = 1
+	)
+	for {
+		if m := p.r_mutx.Up(); m > 0 {
+			//println("sending request")
+			p.event_in <- in
+			//println("sending result")
+			_ = <-p.event_out
+			//println("return result...")
+			p.r_mutx.Down()
 
-		/* 最后一个离开 Request 的 routine需要通知 Runner 可以关闭服务了 */
-		if p.shutdown == true && p.r_mutx.IsClosed() {
-			p.quit <- true
-			//println("final quitting start...")
+			/* 最后一个离开 Request 的 routine需要通知 Runner 可以关闭服务了 */
+			if p.r_mutx.IsClosed() {
+				p.quit <- true
+			}
+			return nil
 		}
-		//println("leaving Requesst ...", p.running)
-		return nil
+		if m == MUTX_ERR_CLOSED {
+			return errors.New("runner closed")
+
+		} else if m == MUTX_ERR_CAP_FULL || m == MUTX_ERR_SYS_BUSY {
+			/* sleep for a few nanoseconds */
+			time.Sleep( time.Duration(slp) * time.Nanosecond)
+			slp += 1
+			if slp > 128 {
+				slp = 1
+			}
+		} else {
+			return errors.New("sys busy")
+		}
 	}
-	return errors.New("sys busy")
 }
 
 
 /* 终止处理器
  */
 func (p *Runner) Quit() error {
-	atomic.AddInt32(&p.quitting, 1)
 	/* 只允许一个routine向 p.quit 发送 信号 */
-	if p.quitting == 1 {
+	if p.q_mutx.Up() > 0 {
 
 		/* 关闭Request, 拒绝后续的请求 */
-		p.shutdown = true
-
-		atomic.AddInt32(&p.running, -1)
-		//println("quiting[", p.running, "]")
-		p.quit <- true
+		p.r_mutx.Down()
+		if p.r_mutx.IsClosed() {
+			p.quit <- true
+		}
 		return nil
 	}
 	return errors.New("runner is shutting down")
@@ -168,31 +167,13 @@ func (p *Runner) run () {
 	for {
 		select {
 
-			/* 收到来自 Request 中的 routine 的终止信号 */
-		case _ = <-p.final_quit:
-			println("quiting done...", p.running)
+			/* 收到终止信号 */
+		case _ = <-p.quit:
+			println("quiting done...")
 			close(p.event_in)
 			close(p.event_out)
 			close(p.quit)
 			return
-
-
-			/* 收到来自 Quit 的终止信号 */
-		case _ = <-p.quit:
-
-			/* 还有routine未离开Request函数 */
-			if p.running >= 0 {
-				/* 最后一个离开Request函数的 routine 发来通知*/
-				//println("final quitting done...")
-				break
-
-			} else {
-				println("quiting done...", p.running)
-				close(p.event_in)
-				close(p.event_out)
-				close(p.quit)
-				return
-			}
 
 			/* 收到请求，处理*/
 		case e := <-p.event_in:
