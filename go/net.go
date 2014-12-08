e6461ffc04eec3da4fffd6e44dc0e32798065a45
awen <awen_eagle@sina.com>
2014-12-08 19:07:27 +0800

    netevent 3.0 interfaces

--- go/net.go
+++ go/net.go
@@ -2,20 +2,23 @@ package	main
 import	"./ne"
 
 func main() {
-	_ = ne.Init()
 	/*
 
 	// global functions 
+
+	ne := ne.Init()
+
 	ne.OnStart = func()error{ return nil }
+
 	ne.OnShutdown = func()error{ return nil }	//catch USR2 signal
+
 	ne.Host = "127.0.0.1"
+
 	ne.Run()
-	ne.Listen(9999, ne.SYNC | ne.ASYNC)
-	ne.Shutdown(9999)
-	ne.Close(fd)
 
 
 	// port functions 
+
 	ne.Port(9999).OnStart = func()error{return nil}
 
 	ne.Port(9999).OnShutdown = func()error{return nil}
@@ -26,25 +29,35 @@ func main() {
 
 	ne.Port(9999).OnRecv = func(fd uint32)error{return nil}
 
+	ne.Port(9999).Listen()
+
+	ne.Port(9999).Shutdown()	???
+
+	ne.Shutdown(9999)	???
+
 
 	// connect functions 
-	ne.Conn(fd).OnRecv = func(fd uint32, pack []byte)error{return nil}
+	ne.NewConnFd()
 
-	ne.Conn(fd).OnClose = func(fd uint32)error{return nil}
+	ne.Conn(fd).OnRecv = func(pack []byte)error{return nil}
+
+	ne.Conn(fd).OnClose = func()error{return nil}
 
 	ne.Conn(fd).OnPackEof = func(stream []byte)(int, error){ return 0, nil }
 
-
 	ne.Conn(fd).Info()
-
 
 	ne.Conn(fd).Connect("127.0.0.1", 8888)
 
-	ne.Conn(fd).Dial("127.0.0.1", 8888)
+	ne.Conn(fd).Watch()
 
 	ne.Conn(fd).Send(data []byte)
 
-	ne.Conn(fd).Close()
+	ne.Conn(fd).Recv(buff []byte)
+
+	ne.Conn(fd).Close()	???
+
+	ne.Close(fd)	???
 
 	*/
 
